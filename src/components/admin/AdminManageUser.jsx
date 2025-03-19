38a6e0c2663f62539028dc48cde2f00b561d752f
jrlyntp <144651413+jrlyntp@users.noreply.github.com>
2025-03-19 19:47:17 +0800

    CreateDeanForm,CreateAdminForm

--- src/components/admin/AdminManageUser.jsx
+++ src/components/admin/AdminManageUser.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import DeanStudentList from "../deanlist/DeanList";
+import CreateDeanForm from "../deanlist/CreateDeanForm"; // Corrected import
+import CreateAdminForm from "../deanlist/CreateAdminForm";
 
 export default function AdminManageUser() {
   return (
-    <div>AdminManageUser</div>
-  )
+    <div>
+      <CreateDeanForm /> 
+      <CreateAdminForm /> {/* Inserted Dean Form */}
+      <DeanStudentList />
+    </div>
+  );
 }
